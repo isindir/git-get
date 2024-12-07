bb4a6947a189caa133e21f6c3b01f8c3dfaa134a
Eriks Zelenka <isindir@users.sf.net>
2024-12-07 18:31:56 +0000

    Fix typo

--- gitget/get.go
+++ gitget/get.go
@@ -1059,7 +1059,7 @@ func writeReposToFile(repoSha string, cfgFile string, repoList []Repo) {
 		}
 
 		log.Infof("%s: Writing file '%s'", repoSha, cfgFile)
-		err = ioutil.WriteFile(cfgFile, repoData, 0o644)
+		err = os.WriteFile(cfgFile, repoData, 0o644)
 		if err != nil {
 			log.Fatalf("%s: %s", cfgFile, err)
 		}
